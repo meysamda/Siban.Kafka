d8d0006c3d0082b1b2a34f5c8ef0707c4c32c3fe
meysam abasi <abasi.meysam@outlook.com>
2021-09-28 00:43:50 +0330

    temp

---
+++ Doc/Samples/Subscriber/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Samples.Subscriber
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {            
+            var brokers = new string[] { "localhost:9092" };
+            services.AddMessageBus(options => options.Brokers = brokers);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Introduction 
+If you are looking for a quick and simple (but strong) message bus to publish messages to Apache Kafka Topics or subscibe on Kafka Topics in your .NET applications, KafkaMessageBus provides you a
