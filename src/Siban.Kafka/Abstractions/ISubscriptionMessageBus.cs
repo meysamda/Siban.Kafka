574f0e9c75a69d58b00b76ce882fcdf4d7aa5aeb
meysam abasi <abasi.meysam@outlook.com>
2022-08-25 20:55:14 +0430

    bug fix

--- src/Siban.Kafka/Abstractions/ISubscriptionMessageBus.cs
+++ src/Siban.Kafka/Abstractions/ISubscriptionMessageBus.cs
@@ -8,6 +8,12 @@ namespace Siban.Kafka.Abstractions
 {    
     public interface ISubscriptionMessageBus
     {
+        Task SubscribeAsync<TMessage>(
+            IEnumerable<string> topics,
+            Func<string, TMessage, Headers, Task> messageProcessor,
+            Action<ISubscribeOptions<string, TMessage>> defaultOptionsModifier = null,
+            CancellationToken cancellationToken = default);
+
         Task SubscribeAsync<TKey, TMessage>(
             IEnumerable<string> topics,
             Func<TKey, TMessage, Headers, Task> messageProcessor,
